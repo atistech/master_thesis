f08f64e121f1a97da6d59993265f2fb168caa9a1
Atakan Senturk <blue.atakan.14@gmail.com>
2023-06-03 03:46:36 +0300

    last changes

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ from flask import Flask, render_template, request, jsonify, redirect, session
 from GeneticAlgorithm import GeneticAlgorithm
 
 app = Flask(__name__)
+ga = object
 
 
 @app.route("/")
 def index():
     return render_template("main.html")
 
-@app.route("/ga-results")
-def ga_results():
-    generationCount = 0
+@app.route("/ga-results/<gen>")
+def ga_results(gen):
+    global ga
     ga = GeneticAlgorithm(param_dict={
         "datasetSelection": "Mnist",
         "populationSize": 2
@@ -18,8 +19,16 @@ def ga_results():
 
     ga.initialPopulation()
     results = ga.calculateFitness()
-    return jsonify(data=[r.serialize() for r in results], gen=generationCount)
+    return jsonify(data=[r.serialize() for r in results])
 
+@app.route("/ga-calback/<gen>")
+def ga_callback(gen):
+    global ga
+    ga.selection()
+    ga.crossOver()
+    ga.mutation()
+    results = ga.calculateFitness()
+    return jsonify(data=[r.serialize() for r in results])
 
 if __name__ == "__main__":
     app.run(debug=True)
