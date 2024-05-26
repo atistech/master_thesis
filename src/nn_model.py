81a56c8ad63e5e1688b706985dad7d86055924b8
Atakan Senturk <blue.atakan.14@gmail.com>
2024-05-27 01:55:04 +0300

    last changes

--- src/nn_model.py
+++ src/nn_model.py
@@ -7,9 +7,13 @@ import src.utils as utils
 import random
 
 class Layer():
-    def __init__(self, units = utils.randomLayerOutputs(), activation = utils.randomLayerActivations()):
-        self.units = units
-        self.activation = activation
+    def __init__(self, isRandom, units = 0, activation = ""):
+        if isRandom:
+            self.units = utils.randomLayerOutputs()
+            self.activation = utils.randomLayerActivations()
+        else:
+            self.units = units
+            self.activation = activation
 
 class NNModel():
     def __init__(self, isRandom, layers, isRegression):
@@ -17,8 +21,8 @@ class NNModel():
             self.layers = []
             layersCount = utils.randomLayerNums()
             for i in range(layersCount):
-                self.layers.append(Layer())
-            self.layers.append(Layer(units=1, activation="sigmoid"))
+                self.layers.append(Layer(isRandom=True))
+            self.layers.append(Layer(isRandom=False, units=1, activation="sigmoid"))
         else:
             self.layers = layers
         self.toKeras()
@@ -38,15 +42,18 @@ class NNModel():
 
     def updateLayersRandomly(self):
         layersLength = len(self.layers)
-        for i in range(random.randint(1, layersLength)):
-            index = random.randint(0, layersLength-1)
-            self.layers[index] = Layer()
+        for i in range(random.randint(1, 4)):
+            if layersLength > 2:
+                index = random.randint(0, layersLength-2)
+            else:
+                index = 0
+            self.layers[index] = Layer(isRandom=True)
 
     def calculateResult(self, dataset):
         if(self.isRegression):
             self.model.compile(
                 optimizer=self.optimizer,
-                loss="binary_crossentropy",
+                loss="mean_absolute_error",
                 metrics=[
                     metrics.MeanAbsolutePercentageError(), 
                     metrics.MeanSquaredError(),
@@ -63,18 +70,16 @@ class NNModel():
                 verbose=0
             )
 
-            print(history.history)
-
-            self.loss = float("%.2f" % history.history['loss'][0])
-            self.val_loss = float("%.2f" % history.history['val_loss'][0])
-            self.mape = float("%.2f" % (history.history['mean_absolute_percentage_error'][0]))
-            self.val_mape = float("%.2f" % history.history['val_mean_absolute_percentage_error'][0])
-            self.mse = float("%.2f" % (history.history['mean_squared_error'][0]))
-            self.val_mse = float("%.2f" % history.history['val_mean_squared_error'][0])
-            self.mae = float("%.2f" % (history.history['mean_absolute_error'][0]))
-            self.val_mae = float("%.2f" % history.history['val_mean_absolute_error'][0])
-            mean = (self.loss + self.val_loss + self.mape + self.val_mape + self.mse + self.val_mse)/6
-            self.fitnessScore = float("%.2f" % mean)
+            self.loss = float("%.4f" % history.history['loss'][-1])
+            self.val_loss = float("%.4f" % history.history['val_loss'][-1])
+            self.mape = float("%.2f" % (history.history['mean_absolute_percentage_error'][-1]))
+            self.val_mape = float("%.2f" % history.history['val_mean_absolute_percentage_error'][-1])
+            self.mse = float("%.2f" % (history.history['mean_squared_error'][-1]))
+            self.val_mse = float("%.2f" % history.history['val_mean_squared_error'][-1])
+            self.mae = float("%.2f" % (history.history['mean_absolute_error'][-1]))
+            self.val_mae = float("%.2f" % history.history['val_mean_absolute_error'][-1])
+            mean = (self.loss + self.val_loss)/2
+            self.fitnessScore = float("%.4f" % mean)
         else:
             self.model.compile(
                 optimizer=self.optimizer,
@@ -90,10 +95,10 @@ class NNModel():
                 verbose=0
             )
 
-            self.loss = float("%.2f" % history.history['loss'][0])
-            self.accuracy = float("%.2f" % (history.history['accuracy'][0]*100))
-            self.val_loss = float("%.2f" % history.history['val_loss'][0])
-            self.val_accuracy = float("%.2f" % (history.history['val_accuracy'][0]*100))
+            self.loss = float("%.2f" % history.history['loss'][-1])
+            self.accuracy = float("%.2f" % (history.history['accuracy'][-1]*100))
+            self.val_loss = float("%.2f" % history.history['val_loss'][-1])
+            self.val_accuracy = float("%.2f" % (history.history['val_accuracy'][-1]*100))
             self.fitnessScore = float("%.2f" % ((self.accuracy+self.val_accuracy)/2))
     
     def architectureString(self):
@@ -105,24 +110,29 @@ class NNModel():
     
     def toDict(self):
         if(self.isRegression):
-            return {
-                'history': f"mape: {self.mape},"+
+            """return {
+                "history": f"Loss: {self.loss},"+
+                f"Validation Loss: {self.val_loss},"+
+                f"mape: {self.mape},"+
                 f"val_mape: {self.val_mape},"+
                 f"mse: {self.mse},"+
                 f"val_mse: {self.val_mse},"+
                 f"mae: {self.mae},"+
-                f"val_mae: {self.val_mae},"+
-                f"loss: {self.loss},"+
-                f"val_loss: {self.val_loss}",
-                'fitnessScore': self.fitnessScore,
-                'architecture': self.architectureString()
+                f"val_mae: {self.val_mae},",
+                "fitnessScore": self.fitnessScore,
+                "architecture": self.architectureString()
+            }"""
+            return {
+                "history": f"Loss: {self.loss},   Validation Loss: {self.val_loss}",
+                "fitnessScore": self.fitnessScore,
+                "architecture": self.architectureString()
             }
         else:
             return {
-                'history': f"accuracy: {self.accuracy},"+
+                "history": f"accuracy: {self.accuracy},"+
                 f"val_accuracy: {self.val_accuracy},"+
                 f"loss: {self.loss},"+
                 f"val_loss: {self.val_loss}",
-                'fitnessScore': self.fitnessScore,
-                'architecture': self.architectureString()
+                "fitnessScore": self.fitnessScore,
+                "architecture": self.architectureString()
             }
